4a20614d665562c57f49cd916359b76706f85857
DELL <DELL@DESKTOP-MIVUQIR>
2024-11-15 13:41:33 +0530

    Second Commit

---
+++ GitHub/src/main/java/GitHub/GitHub.java
@@ -0,0 +1,16 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHub {
+
+	@Test
+	public void addMessage() {
+		Reporter.log("Hii");
+	}
+	
+	
+	
+
+}
